bdd1c6e8c375633c9ac5c67d948fa8a7e9af7f1a
Samuel Favarin <favarin.dev@gmail.com>
2023-01-18 18:00:45 +0000

    chore: update dockerfile and include the manifest.json

--- Dockerfile
+++ Dockerfile
@@ -11,12 +11,16 @@ COPY pyproject.toml /app/
 # Add credentials for paperlabs library
 RUN apt-get -yq update \
     && apt-get install -y git \
-    && apt-get install -y gcc
+    && apt-get install -y gcc \
+    && apt-get install -y jq \
+    && apt-get install -y curl 
 
 RUN poetry export --extras dbt --without-hashes -f requirements.txt --output /app/requirements.txt
 RUN pip install --no-cache-dir -r /app/requirements.txt
 
-
 COPY . /app/
 
-ENTRYPOINT ["dbt", "build", "--select", "tag:daily"]
+RUN chmod +x ./get_manifest.sh
+ENTRYPOINT ./get_manifest.sh
+
+ENTRYPOINT "dbt build --select tag:daily --defer --state ./"
---
+++ get_manifest.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# GET ACCOUNT ID
+#curl --request GET \
+# --url https://cloud.getdbt.com/api/v2/accounts/ \
+# --header 'Content-Type: application/json' \
+# --header 'Authorization: Token <your-token>'
+account_id=10829
+
+# GET RUN ID
+#last_run_id=$(curl --request GET \
+#  --url https://cloud.getdbt.com/api/v2/accounts/$account_id/runs/?limit=1\&order_by=-id \
+#  --header 'Content-Type: application/json' \
+#  --header 'Authorization: Token <your-token>' | jq -r '.data[0] .id');
+
+#echo "LAST RUN ID: " $last_run_id 
+
+# GET MANIFEST.JSON
+#curl --request GET \
+#  --url https://cloud.getdbt.com/api/v2/accounts/{$account_id}/runs/{$last_run_id}/artifacts/manifest.json \
+#  --header 'Content-Type: application/json' \
+#  --header 'Authorization: Token <your-token>' >> /app/manifest.json
+
+echo "MANIFEST.JSON GENERATED" 
+
